3c6be1c50ad05af90b9831872d1139ce5753f211
oxreng <merkurev06@mail.ru>
2023-09-13 18:12:28 +0300

    pogoda_bot

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 class Config:
-    TOKEN = '6335088887:AAHYiWdiuerjFKH8sMZe0xbOXodd9DE_Nm0'
+    TOKEN = '6624992385:AAEk1t9ufuKwkYcWLrMzJQHXqd3cJ3hSmCM'
+    KEY = 'c465369b9521e340aa8cf1965beadec3'
--- main.py
+++ main.py
@@ -1,35 +1,70 @@
 from config import Config
 import logging
+import requests
 from aiogram import Bot, Dispatcher, executor, types
-import aiogram.utils.markdown as fmt
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
-bot = Bot(token=Config.TOKEN, parse_mode=types.ParseMode.MARKDOWN_V2)
+bot = Bot(token=Config.TOKEN)
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
+dp.middleware.setup(LoggingMiddleware())
+
+cities = [
+    "Москва",
+    "Санкт-Петербург",
+    "Новосибирск",
+    "Екатеринбург",
+    "Нижний Новгород",
+    "Казань",
+    "Челябинск",
+    "Омск",
+    "Самара",
+    "Ростов-на-Дону",
+]
 
 
-@dp.message_handler(text='1')
-async def cmd_test_1(message: types.Message):
-    await message.reply(f'Привет *{message.from_user.username}*')
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    '''Здоровается и показывает кнопку "Weather"'''
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    button = types.KeyboardButton('Weather')
+    keyboard.add(button)
+    await message.answer('Привет, я бот с погодой нажми Weather, чтобы выбрать город!', reply_markup=keyboard)
 
 
-@dp.message_handler(text='2')
-async def cmd_text_2(message: types.Message):
-    await message.answer(
-        fmt.text(
-            fmt.text(fmt.hunderline('Яблоки'), 'вес 1 кг\n'),
-            fmt.text('Старая цена: ', fmt.hstrikethrough(50), 'рублей\n'),
-            fmt.text('Новая цена: ', fmt.hbold(25), 'рублей')
-        ), parse_mode=types.ParseMode.HTML
-    )
+@dp.message_handler(lambda message: message.text.lower() == 'weather')
+async def cmd_weather(message: types.Message):
+    """Отправляет пользователю список городов для выбора погоды"""
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    buttons = [types.KeyboardButton(city) for city in cities]
+    keyboard.add(*buttons)
+    await message.answer('Выберите город, чтобы узнать погоду', reply_markup=keyboard)
 
 
-@dp.message_handler(text='Три')
-async def cmd_text_3(message: types.Message):
-    await message.answer(message.text)
-    await message.answer(message.md_text)
-    await message.answer(message.html_text)
-    await message.answer(f'<u>Ваш текст</u>:\n\n{message.html_text}', parse_mode='HTML')
+@dp.message_handler(lambda message: message.text in cities)
+async def get_weather(message: types.Message):
+    """Получает погоду для выбранного города и отправляет юзеру"""
+    city = message.text
+    api_key = Config.KEY
+    weather_api_url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru'
+
+    try:
+        r = requests.get(weather_api_url)
+        data = r.json()
+
+        if r.status_code == 200:
+            temperature = data['main']['temp']
+            weather_desc = data['weather'][0]['description']
+            speed_wind = data['wind']['speed']
+
+            await message.answer(f'Погода в городе {city}:\nТемпепатура: {temperature}°C\n'
+                                 f'Описание: {weather_desc}\nСкорость ветра: {speed_wind} м/с')
+
+        else:
+            await message.answer('Не удалось получить данные :(')
+    except Exception as e:
+        await message.answer('Произошла ошибка при получении данных')
+        logging.error(e)
 
 
 if __name__ == '__main__':
