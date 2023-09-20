b71fce08df1ce2cdb7a4a374dcfac78eb7f599e5
oxreng <merkurev06@mail.ru>
2023-09-20 17:24:58 +0300

    DEL_KB

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from config import Config
+from aiogram.types import ReplyKeyboardRemove
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from keyboards import kb_drink, kb_start, kb_zak
+from keyboards import kb
 
 bot = Bot(token=Config.TOKEN)
 dp = Dispatcher(bot)
@@ -10,29 +11,14 @@ logging.basicConfig(level=logging.INFO)
 dp.middleware.setup(LoggingMiddleware())
 
 
-@dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
-    await message.reply('Привет, лови мой ассортимент!', reply_markup=kb_start)
+@dp.message_handler(commands='start')
+async def cmd_start(message: types.Message):
+    await message.reply('Привет', reply_markup=kb)
 
 
-@dp.message_handler(lambda message: message.text.lower().startswith('закуски'))
-async def zak_list(message: types.Message):
-    await message.answer('Держи все закуски из моего справочника!', reply_markup=kb_zak)
-
-
-@dp.message_handler(lambda message: message.text.lower().startswith('напитки'))
-async def drink_list(message: types.Message):
-    await message.answer('Держи все напитки из моего справочника!', reply_markup=kb_drink)
-
-
-@dp.message_handler(lambda message: message.text.lower().startswith('назад'))
-async def back(message: types.Message):
-    await message.answer('Хорошо, возвращаюсь в начальное меню', reply_markup=kb_start)
-
-
-@dp.message_handler()
-async def another_answer(message: types.Message):
-    await message.answer('Отстань от меня пж, пж, пж....')
+@dp.message_handler(commands='rm')
+async def cmd_rm(message: types.Message):
+    await message.reply('Убрать клавиатуру', reply_markup=ReplyKeyboardRemove())
 
 
 if __name__ == '__main__':
