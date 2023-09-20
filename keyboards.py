bdadc4185ec0d9c4f5f5d8a17c51d7539b078de6
oxreng <merkurev06@mail.ru>
2023-09-20 18:21:04 +0300

    INLINE

--- keyboards.py
+++ keyboards.py
@@ -5,9 +5,28 @@ btn_2 = KeyboardButton('Пока!')
 kb = ReplyKeyboardMarkup(resize_keyboard=True)
 kb.add(btn_1, btn_2)
 
-inline_btn_1 = InlineKeyboardButton('Первая кнопка', callback_data='button_1')
+inline_btn_0 = InlineKeyboardButton('Первая кнопка', callback_data='button_0')
 inline_kb = InlineKeyboardMarkup()
-inline_kb.add(inline_btn_1)
+inline_kb.add(inline_btn_0)
+
+inline_btn_1 = InlineKeyboardButton('Первая кнопка', callback_data='btn_1')
+inline_btn_2 = InlineKeyboardButton('Вторая кнопка', callback_data='btn_2')
+inline_btn_3 = InlineKeyboardButton('Третья кнопка', callback_data='btn_3')
+inline_btn_4 = InlineKeyboardButton('Четвёртая кнопка', callback_data='btn_4')
+inline_btn_5 = InlineKeyboardButton('Пятая кнопка', callback_data='btn_5')
+inline_kb_full = InlineKeyboardMarkup(row_width=2)
+
+inline_kb_full.add(inline_btn_1, inline_btn_2)
+inline_kb_full.row(inline_btn_3, inline_btn_4, inline_btn_5)
+
+inline_kb_full.insert(InlineKeyboardButton('query', switch_inline_query=''))
+inline_kb_full.insert(InlineKeyboardButton('Yandex', url='https://www.yandex.ru'))
+
+
+
+
+
+
 
 
 # kb.add(button_1)
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from aiogram.types import ReplyKeyboardRemove
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from keyboards import kb
+from keyboards import kb, inline_kb, inline_kb_full
+import re
 
 bot = Bot(token=Config.TOKEN)
 dp = Dispatcher(bot)
@@ -13,12 +14,20 @@ dp.middleware.setup(LoggingMiddleware())
 
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
-    await message.reply('Привет', reply_markup=kb)
+    await message.reply('Отправляю кнопку', reply_markup=inline_kb_full)
 
 
-@dp.message_handler(commands='rm')
-async def cmd_rm(message: types.Message):
-    await message.reply('Убрать клавиатуру', reply_markup=ReplyKeyboardRemove())
+@dp.callback_query_handler(lambda callback_query: re.match(r'^btn.{0,10}$', callback_query.data))
+async def process_callback_kb_full(callback_query: types.CallbackQuery):
+    code = callback_query.data[-1]
+    print(code)
+    if code.isdigit():
+        code = int(code)
+    if code == 1:
+        await bot.answer_callback_query(callback_query.id, text='Нажата 1 кнопка')
+    else:
+        await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(callback_query.from_user.id, f'Нажата инлайн кнопка {code}')
 
 
 if __name__ == '__main__':
