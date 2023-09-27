2c8c07f6990939d2e452b6711f307c9573cc06cc
oxreng <merkurev06@mail.ru>
2023-09-27 18:18:20 +0300

    MINIGAME GUESS A NUM

--- main.py
+++ main.py
@@ -16,38 +16,62 @@ dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 dp.middleware.setup(LoggingMiddleware())
 
-
-def get_inline_kb():
-    inline_kb = InlineKeyboardMarkup()
-    btns = [InlineKeyboardButton('1', callback_data='1'),
-            InlineKeyboardButton('2', callback_data='2')]
-    inline_kb.add(*btns)
-    return inline_kb
+secret_num = None
+attempts = None
 
 
-def get_inline_kb_1():
-    inline_kb = InlineKeyboardMarkup()
-    btns = [InlineKeyboardButton('вперед', callback_data='forward'),
-            InlineKeyboardButton('закрыть', callback_data='back')]
-    inline_kb.add(*btns)
-    return inline_kb
+def random_num():
+    return randint(1, 10)
+
+
+def kb():
+    start_menu = InlineKeyboardMarkup(resize_keyboard=True)
+    button_start = InlineKeyboardButton('Начать', callback_data='start_game')
+    button_stop = InlineKeyboardButton('Отмена', callback_data='cancel')
+    start_menu.add(button_start, button_stop)
+    return start_menu
 
 
 @dp.message_handler(commands='start')
-async def menu_bot(message: types.Message):
-    await message.answer('Это инлайн клава', reply_markup=get_inline_kb())
+async def cmd_start(message: types.Message):
+    await message.answer('Привет, это игра "Угадай число"', reply_markup=kb())
 
 
-@dp.callback_query_handler(text='1')
-async def btn_1(callback: types.CallbackQuery):
-    await callback.answer()
-    await callback.message.answer('Нажата кнопка 1', reply_markup=get_inline_kb_1())
+@dp.callback_query_handler(text='cancel')
+async def end_game(call: types.CallbackQuery):
+    await call.answer()
+    await call.message.delete()
 
 
-@dp.callback_query_handler(text='back')
-async def btn_back(callback: types.CallbackQuery):
-    await callback.answer()
-    await callback.message.answer('Вышли в главное меню', reply_markup=get_inline_kb())
+@dp.callback_query_handler(text='start_game')
+async def start_game(call: types.CallbackQuery):
+    global secret_num, attempts
+    attempts = 3
+    secret_num = random_num()
+    await call.message.answer('Я загадал число от 1 до 10, попробуй отгадать')
+    await call.answer()
+    await call.message.delete()
+    print(secret_num)
+
+
+@dp.message_handler(regexp=re.compile(r'^\d+$'))
+async def check_numbers(message: types.Message):
+    global secret_num, attempts
+    user_number = int(message.text)
+    attempts -= 1
+    if secret_num != None:
+        if user_number > secret_num:
+            await message.reply(f'Ты не угадал! Моё число меньше, оставшееся кол-во попыток {attempts}')
+        elif user_number < secret_num:
+            await message.reply(f'Ты не угадал! Моё число больше, оставшееся кол-во попыток {attempts}')
+        else:
+            await message.reply(f'Ты угадал! Поздравляю!!! Начать заново?', reply_markup=kb())
+            secret_num = None
+            attempts = None
+    else:
+        await message.reply('Для начала игры нажмите старт')
+    if attempts < 1:
+        await message.reply('Вы проиграли', reply_markup=kb())
 
 
 if __name__ == '__main__':
