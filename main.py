c6a1403e9041943ba22c2b8e9995f93c8a4fe7c9
oxreng <merkurev06@mail.ru>
2023-10-02 17:03:07 +0300

    DRINKS AND FOOD WITH STATE

--- main.py
+++ main.py
@@ -2,76 +2,76 @@ from config import Config
 import logging
 from contextlib import suppress
 from aiogram.dispatcher.filters import CommandStart
-from aiogram.types import BotCommandScopeDefault, BotCommand, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import BotCommandScopeDefault, BotCommand, InlineKeyboardButton, \
+    InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.utils.callback_data import CallbackData
 from aiogram.utils.exceptions import MessageNotModified
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from filters import AdminFilter
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from random import randint
 import re
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
 bot = Bot(token=Config.TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 logging.basicConfig(level=logging.INFO)
 dp.middleware.setup(LoggingMiddleware())
 
-secret_num = None
-attempts = None
+food_names = ['суши', 'паста', 'устрицы', 'лагман']
+food_size = ['маленькую', 'среднюю', 'большую']
 
 
-def random_num():
-    return randint(1, 10)
+class OrderFood(StatesGroup):
+    waiting_for_food_name = State()
+    waiting_for_food_size = State()
 
 
-def kb():
-    start_menu = InlineKeyboardMarkup(resize_keyboard=True)
-    button_start = InlineKeyboardButton('Начать', callback_data='start_game')
-    button_stop = InlineKeyboardButton('Отмена', callback_data='cancel')
-    start_menu.add(button_start, button_stop)
-    return start_menu
+@dp.message_handler(commands='start', state='*')
+async def cmd_start(message: types.Message, state: FSMContext):
+    await state.finish()
+    await message.answer('выберите что хотите заказать /food, /drinks', reply_markup=ReplyKeyboardRemove())
 
 
-@dp.message_handler(commands='start')
-async def cmd_start(message: types.Message):
-    await message.answer('Привет, это игра "Угадай число"', reply_markup=kb())
+@dp.message_handler(commands='cancel', state='*')
+async def cmd_start(message: types.Message, state: FSMContext):
+    await state.finish()
+    await message.answer('действие отменено', reply_markup=ReplyKeyboardRemove())
 
 
-@dp.callback_query_handler(text='cancel')
-async def end_game(call: types.CallbackQuery):
-    await call.answer()
-    await call.message.delete()
+@dp.message_handler(commands='food', state='*')
+async def food_start(message: types.Message):
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    for name in food_names:
+        kb.add(name)
+    await message.answer('Выберите блюдо', reply_markup=kb)
+    await OrderFood.waiting_for_food_name.set()
 
 
-@dp.callback_query_handler(text='start_game')
-async def start_game(call: types.CallbackQuery):
-    global secret_num, attempts
-    attempts = 3
-    secret_num = random_num()
-    await call.message.answer('Я загадал число от 1 до 10, попробуй отгадать')
-    await call.answer()
-    await call.message.delete()
-    print(secret_num)
+@dp.message_handler(state=OrderFood.waiting_for_food_name)
+async def food_choise(message: types.Message, state: FSMContext):
+    if message.text.lower() not in food_names:
+        await message.answer('Пожалуйста, выбери блюдо с клавиатуры')
+        return
+    await state.update_data(food_choise=message.text.lower())
+
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    for size in food_size:
+        kb.add(size)
+    await OrderFood.next()
+    await message.answer('Выберите порцию', reply_markup=kb)
 
 
-@dp.message_handler(regexp=re.compile(r'^\d+$'))
-async def check_numbers(message: types.Message):
-    global secret_num, attempts
-    user_number = int(message.text)
-    attempts -= 1
-    if secret_num != None:
-        if user_number > secret_num:
-            await message.reply(f'Ты не угадал! Моё число меньше, оставшееся кол-во попыток {attempts}')
-        elif user_number < secret_num:
-            await message.reply(f'Ты не угадал! Моё число больше, оставшееся кол-во попыток {attempts}')
-        else:
-            await message.reply(f'Ты угадал! Поздравляю!!! Начать заново?', reply_markup=kb())
-            secret_num = None
-            attempts = None
-    else:
-        await message.reply('Для начала игры нажмите старт')
-    if attempts < 1:
-        await message.reply('Вы проиграли', reply_markup=kb())
+@dp.message_handler(state=OrderFood.waiting_for_food_size)
+async def size_choise(message: types.Message, state: FSMContext):
+    if message.text.lower() not in food_size:
+        await message.answer('Пожалуйста, выберите порцию с клавиатуры')
+        return
+    user_data = await state.get_data()
+    await message.answer(f'Вы заказали {message.text.lower()} порцию {user_data["food_choise"]}'
+                         f'\nПопробуйте теперь заказать напитки', reply_markup=ReplyKeyboardRemove())
 
 
 if __name__ == '__main__':
